6b5de12950e9c4a763293a60469ab815d6e1fc60
jonghan.kim <jonghan.kim@cdnetworks.co.kr>
2017-11-19 18:10:59 +0900

    Footer divided

--- src/layouts/HomepageLayout.js
+++ src/layouts/HomepageLayout.js
@@ -3,12 +3,13 @@ import { Button, Container, Divider, Grid, Header,
         Icon, Image, List, Menu, Segment, Visibility,}
         from 'semantic-ui-react'
 
+import Footer from '../components/Footer'
+
 const FixedMenu = () => (
   <Menu fixed='top' size='large'>
     <Container>
       <Menu.Item as='a' active>Home</Menu.Item>
       <Menu.Item as='a'>Work</Menu.Item>
-      <Menu.Item as='a'>Company</Menu.Item>
       <Menu.Item as='a'>Careers</Menu.Item>
       <Menu.Menu position='right'>
         <Menu.Item className='item'>
@@ -21,6 +22,11 @@ const FixedMenu = () => (
     </Container>
   </Menu>
 )
+/**
+ * When Height is low, this Menu will be shown.
+ */
+
+
 
 export default class HomepageLayout extends Component {
   state = {}
@@ -50,7 +56,6 @@ export default class HomepageLayout extends Component {
               <Menu inverted pointing secondary size='large'>
                 <Menu.Item as='a' active>Home</Menu.Item>
                 <Menu.Item as='a'>Work</Menu.Item>
-                <Menu.Item as='a'>Company</Menu.Item>
                 <Menu.Item as='a'>Careers</Menu.Item>
                 <Menu.Item position='right'>
                   <Button as='a' inverted>Log in</Button>
@@ -158,36 +163,7 @@ export default class HomepageLayout extends Component {
           </Container>
         </Segment>
 
-        <Segment inverted vertical style={{ padding: '5em 0em' }}>
-          <Container>
-            <Grid divided inverted stackable>
-              <Grid.Row>
-                <Grid.Column width={3}>
-                  <Header inverted as='h4' content='About' />
-                  <List link inverted>
-                    <List.Item as='a'>Sitemap</List.Item>
-                    <List.Item as='a'>Contact Us</List.Item>
-                    <List.Item as='a'>Religious Ceremonies</List.Item>
-                    <List.Item as='a'>Gazebo Plans</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column width={3}>
-                  <Header inverted as='h4' content='Services' />
-                  <List link inverted>
-                    <List.Item as='a'>Banana Pre-Order</List.Item>
-                    <List.Item as='a'>DNA FAQ</List.Item>
-                    <List.Item as='a'>How To Access</List.Item>
-                    <List.Item as='a'>Favorite X-Men</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column width={7}>
-                  <Header as='h4' inverted>Footer Header</Header>
-                  <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Container>
-        </Segment>
+        <Footer />
       </div>
     )
   }
