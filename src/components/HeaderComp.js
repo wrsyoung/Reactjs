24f8fd0a7c0e2543b854eef1e3e29d58fba3dcb4
jonghan.kim <jonghan.kim@cdnetworks.co.kr>
2017-11-21 15:06:59 +0900

    HeaderComponent to Class

--- src/components/HeaderComp.js
+++ src/components/HeaderComp.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import { Button, Container, Menu, } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
-const HeaderComp = () => (
-  <Container>
-    <Menu pointing secondary size='large'>
-      <Menu.Item as='a' active>Home</Menu.Item>
-      <Menu.Item as='a'>Work</Menu.Item>
-      <Menu.Item as='a'>Careers</Menu.Item>
-      <Menu.Item position='right'>
-        <Button as='a'>Log in</Button>
-        <Button as='a' style={{ marginLeft: '0.5em' }} primary>Sign Up</Button>
-      </Menu.Item>
-    </Menu>
-  </Container>
-);
+class HeaderComp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Container>
+          <Menu pointing secondary size='large'>
+            <Menu.Item as={Link} to='/' active>Home</Menu.Item>
+            <Menu.Item as={Link} to='/work'> Work</Menu.Item>
+            <Menu.Item as='a'>Careers</Menu.Item>
+            <Menu.Item position='right'>
+              <Button as='a'>Log in</Button>
+              <Button as='a' style={{ marginLeft: '0.5em' }} primary>Sign Up</Button>
+            </Menu.Item>
+          </Menu>
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default HeaderComp;
